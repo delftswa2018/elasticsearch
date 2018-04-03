b0063bc73319024f30421b22477fe052f9f76eb8
MathiasMeuleman <m.meuleman@student.tudelft.nl>
2018-04-03 11:34:19 +0200

    Remove all tests and add sonar plugin

---
+++ server/src/test/java/org/elasticsearch/TrivialTest.java
@@ -0,0 +1,10 @@
+package org.elasticsearch;
+
+import org.elasticsearch.test.ESTestCase;
+
+public class TrivialTest extends ESTestCase{
+
+    public void trivialTest() {
+        assertEquals(true, true);
+    }
+}
